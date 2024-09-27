64a8a413749d0d296e45d7adecea432db1df8c19
Luis F. da S. H. Pires <luis.pires01@fatec.sp.gov.br>
2024-09-27 10:54:07 -0300

    feature: src/components/QuizzLoader.tsx
    Creating loader component to project.

---
+++ src/components/QuizzLoader.tsx
@@ -0,0 +1,9 @@
+const QuizzLoader = () => {
+    return (
+        <div className="flex justify-center">
+            <span className="spinner"></span>
+        </div>
+    )
+};
+
+export default QuizzLoader;
