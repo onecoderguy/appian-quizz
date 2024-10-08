c4b98ef82107f15f5a7ec86bc0c6d188adba80a3
Luis F. da S. H. Pires <luis.pires01@fatec.sp.gov.br>
2024-10-08 16:17:26 -0300

    feature: src/interfaces/QuizzOldResultData.ts

---
+++ src/interfaces/QuizzOldResultData.ts
@@ -0,0 +1,11 @@
+interface QuizzOldResultData {
+    id: number,
+    date: string,
+    topics: string,
+    duration: number,
+    totalQuestions: number,
+    unansweredQuestions: number,
+    correctAnswers: number
+};
+
+export default QuizzOldResultData;
